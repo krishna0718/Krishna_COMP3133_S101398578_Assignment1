21b0a2bc797eed5e2d5d59ac1ff9e214e90ffb82
krish <krishnathkkar072004@gmail.com>
2025-02-14 16:45:00 -0500

    assignment1 final submission

--- Employee_management_system_backend/models/Employee.js
+++ Employee_management_system_backend/models/Employee.js
@@ -1,26 +1,3 @@
-/*const mongoose = require("mongoose");
-
-const EmployeeSchema = new mongoose.Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
-  designation: { type: String, required: true },
-  salary: { type: Number, required: true, min: 1000 },
-  date_of_joining: { type: Date, required: true },
-  department: { type: String, required: true },
-  employee_photo: { type: String },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Employee", EmployeeSchema);
-*/
-
-
-
-
-
 const mongoose = require("mongoose");
 
 const EmployeeSchema = new mongoose.Schema({
@@ -37,7 +14,7 @@ const EmployeeSchema = new mongoose.Schema({
   updated_at: { type: Date, default: Date.now }
 });
 
-// Middleware to update `updated_at` automatically before saving
+
 EmployeeSchema.pre("findOneAndUpdate", function (next) {
   this.set({ updated_at: new Date() });
   next();
--- Employee_management_system_backend/server.js
+++ Employee_management_system_backend/server.js
@@ -1,35 +1,3 @@
-/*const express = require('express')
-const mongoose = require('mongoose')
-const { ApolloServer } = require("apollo-server-express");
-const bodyParser = require("body-parser");
-const cors = require('cors')
-const dotenv = require('dotenv').config()
-const connectDB = require('./config/db.js')
-const typeDefs = require("./graphql/typeDefs");
-const resolvers = require("./graphql/resolvers");
-
-
-
-//Calling the database
-connectDB()
-
-
-
-
-const app = express();
-app.use(bodyParser.json());
-app.use('*', cors());
-server.applyMiddleware({ app });
-app.listen({ port: process.env.PORT }, () =>
-  console.log(`:rocket: Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`))
-*/
-
-
-
-
-
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const { ApolloServer } = require("apollo-server-express");
